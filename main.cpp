ca432ac11d126e467ab55be423eb407059e84e72
QAQbeginner <72873636+QAQbeginner@users.noreply.github.com>
2023-08-04 19:09:09 +0800

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -3,33 +3,46 @@
 void xunhuan(BplusTree *m_tree,vector<PNodeInfo> &node_arr);
 int main()
 {
-	int root_node_id=0;
-	int node_num=0;
-	init(root_node_id,node_num);
+	int root_node_id=1;
+	int node_num=1;
+	bool is_kong=true;
+	init(root_node_id,node_num,is_kong);
 	vector<PNodeInfo> node_arr;
 	for(int i=0;i<=node_num;i++)
-	{
+	{ 
 		PNodeInfo t_node_info=new NodeInfo();
 		node_arr.push_back(t_node_info);
 	}
 	// vector<PNodeInfo> node_arr(node_num+1,new NodeInfo());
 	// vector<PNodeInfo>* node_arr=new vector<PNodeInfo>[node_num];
 	BplusTree *m_tree=new BplusTree();
-	m_tree->root=BplusTree::load_node(root_node_id,node_arr);
+	if(is_kong==true)
+	{
+		node_arr[1]->node=new BPlusTreeNode();
+		node_arr[1]->node->is_leaf=true;
+		node_arr[1]->node->id=node_num;
+		node_arr[1]->is_loaded=true;
+		m_tree->root=node_arr[1]->node;
+	}
+	else
+		m_tree->root=BplusTree::load_node(root_node_id,node_arr);
+	m_tree->node_num=node_num;
 	xunhuan(m_tree,node_arr);
 }
 void xunhuan(BplusTree *m_tree,vector<PNodeInfo> &node_arr)
 {
 	while (true)
 	{
+		//system("clear");
 		char choose = 0;
 		cout << "please input your choose" << endl;
-		cout << "1:insert 2:find 3:all" << endl;
+		cout << "1:insert 2:find 3:all 4:save 5:change 6:delect" << endl;
 		choose = getchar();
 		switch (choose)
 		{
 		case '1':
 		{
+			system("clear");
 			int score;
 			string name;
 			cout << "please input your score" << endl;
@@ -41,28 +54,32 @@ void xunhuan(BplusTree *m_tree,vector<PNodeInfo> &node_arr)
 		}break;
 		case '2':
 		{
+			system("clear");
 			int score;
 			cout << "please input your find_score" << endl;
 			cin >> score;
 			m_tree->search(m_tree->root, score,node_arr);
 		}break;
 		case '3':
-		{
-			m_tree->all(m_tree->root);
+		{ 
+			system("clear");
+			m_tree->all(m_tree->root,node_arr);
 		}break;
 		case '4':
 		{
+			system("clear");
 			m_tree->save_all(m_tree->root);
 		}break;
 		case '5':
 		{
-			// int fp=open("./data/node1.txt",O_RDWR);
-			string path="./data/node1.txt";
-			// load_node(path);
-			return ;
-		}
+			system("clear");
+			int key;
+			cin>>key;
+			m_tree->change(m_tree->root,key,node_arr);
+		}break;
 		case '6':
 		{
+			system("clear");
 			int key;
 			cout<<"input your key"<<endl;
 			cin>>key;
